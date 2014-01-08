7bd48e40a557b5d30baa20293ac317a8def8daa5
BlandinePuybaudet <blandinoscope@orange.fr>
2014-01-08 15:48:31 +0100

    beer song ok

--- Wagon-1/03-Beer-song/beer_song.rb
+++ Wagon-1/03-Beer-song/beer_song.rb
@@ -1,13 +1,27 @@
-def beersong(nb_at_start)
-  # your code goes here
+def beersong(nb)
+ 	while nb>2
+ puts "#{nb} bottles of beer on the wall, #{nb} bottles of beer!" 
+ puts "Take one down, pass it around, #{nb -1} bottles of beer on the wall!"
+ nb -= 1
+	end
 end
 
-beersong( ARGV[0] )
+def beersong_end
+puts "Take one down, pass it around, 1 bottle of beer on the wall!"	
+puts "1 bottle of beer on the wall, 1 bottle of beer!"
+puts "Take one down, pass it around, no more bottles of beer on the wall!"
+end
+
+nb = 10
+beersong(nb)
+beersong_end
+
+
 
 # output (if called with 5 beers) :
 
 # => 5 bottles of beer on the wall, 5 bottles of beer!
-# => Take one down, pass it around, 4 bottles of beer on the wall!
+# => 
 # => 4 bottles of beer on the wall, 4 bottles of beer!
 # => Take one down, pass it around, 3 bottles of beer on the wall!
 # => 3 bottles of beer on the wall, 3 bottles of beer!
