32bc57481973ecee6f673c699cea621fb16fcf47
BlandinePuybaudet <blandinoscope@orange.fr>
2014-01-08 12:14:03 +0100

    ex Wagon-1 modif

--- Wagon-1/01-Simple-looping/simple_looping.rb
+++ Wagon-1/01-Simple-looping/simple_looping.rb
@@ -1,12 +1,41 @@
 # The objective is to create a function that computes the sum of the integers from a min value to a max value
 
 def sum(min,max)
-  # your code here
+	result = 0
+	i = min
+ 	
+ 	while i <= max  
+ 		result += i
+ 		i += 1
+ 	end
+
+ 	result
+end
+
+
+def sum_for(min,max)
+	result = 0
+	for i in (min..max)
+		result += i
+	end
+	result
+end
+
+def sum_recursive(min,max)
+	if min == max
+		return max
+	else
+		min + sum_recursive(min + 1, max)
+	end
 end
 
 # Testing your code
 
 min = 1
 max = 100
-sum = sum(1,100)
-puts sum == 5050 # => true
+sum = sum(min, max)
+puts sum == 5050
+
+
+puts(sum_for(min, max) == 5050)
+puts(sum_recursive(min, max) == 5050)
--- Wagon-1/02-Sorting-the-wagon/wagon_sort.rb
+++ Wagon-1/02-Sorting-the-wagon/wagon_sort.rb
@@ -1,5 +1,22 @@
 def wagon_sort( students_array )
-  # your code to sort the array and output students
+ students_array.sort_by {|x| x.downcase} # your code to sort the array and output students
 end
 
-# your code to interact with the user..
+def start_game
+	students_array= []
+
+	while true
+		puts "entrez votre nom"
+		student = gets.chomp
+		break if student.empty?
+		students_array << student
+	end
+
+	students_count = students_array.length
+	student_string = (students_count > 1) ? "students" : "student"
+
+	puts "Congrats ! Your Wagon has #{students_count} #{student_string}"
+  puts wagon_sort(students_array)
+end
+
+start_game
