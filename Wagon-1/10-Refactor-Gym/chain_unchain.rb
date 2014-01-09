baa1edd94def5b20db73c96da935af031196e20f
BlandinePuybaudet <blandinoscope@orange.fr>
2014-01-09 15:58:25 +0100

    refactor-gym new vrs

--- Wagon-1/10-Refactor-Gym/chain_unchain.rb
+++ Wagon-1/10-Refactor-Gym/chain_unchain.rb
@@ -1,20 +1,27 @@
 def shuffle_word(a_word)
   # Ruby fonction to play to "Des chiffres et des lettres.."
   
-  cap_word = a_word.upcase
-  chars_enum = cap_word.chars
-  array = chars_enum.to_a
-  shuffle_array = array.shuffle
+  #cap_word = a_word.upcase
+  #chars_enum = cap_word.chars
+  #array = chars_enum.to_a
+  #shuffle_array = array.shuffle
+ shuffle_array= a_word.upcase.chars.to_a.shuffle
   
   return shuffle_array
   
 end
 
+def meth (j)
+(2..j-1).select {|k| j % k == 0 }
+end
+
 
 def quote_prime_numbers(n)
-  (1..n).find_all {|i| (2..i-1).select {|k| i % k == 0 }.count == 0 }.map{ |prime_num| "#{prime_num} is prime"}
+	a = (1..n).find_all {|i| meth(i).count == 0 }
+    a.map{ |b| "#{b} is prime"}
 end
 
 
 puts shuffle_word("nabuchodonosor")
 puts quote_prime_numbers(30)
+
