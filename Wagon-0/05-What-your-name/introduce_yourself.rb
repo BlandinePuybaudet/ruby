62374263c480fda8f126c06f9de8f785cf7790f6
BlandinePuybaudet <blandinoscope@orange.fr>
2014-01-10 16:19:07 +0100

    what's your name revue

--- Wagon-0/05-What-your-name/introduce_yourself.rb
+++ Wagon-0/05-What-your-name/introduce_yourself.rb
@@ -1,5 +1,30 @@
-puts "quel est ton nom ?"
-nom=gets.chomp
+
+
+
+=begin
+
+def salut()
+	puts"Quel est ton prenom?"
+	prenom = gets.chomp
+	puts "Et quel est ton nom?"
+	nom = gets.chomp
+ "Salut #{prenom} #{nom} !"
+end
+
+ puts salut
+=end
+
+
+def salut_bis()
 puts"Quel est ton prenom?"
-prenom=gets.chomp
-puts " Hello #{nom} #{prenom} !"
+prenom = gets.chomp
+puts "Et quel est ton nom?"
+nom = gets.chomp
+
+prenom_carac = prenom.length
+nom_carac = nom.length
+
+ "Salut #{prenom} #{nom} ; ton prenom contient : #{prenom_carac} lettres et ton nom en contient : #{nom_carac} "
+end	
+
+puts salut_bis
