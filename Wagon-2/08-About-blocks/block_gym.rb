7c5adad3c043acfb0b769f9fda8faf852cf143e6
BlandinePuybaudet <blandinoscope@orange.fr>
2014-01-12 17:35:57 +0100

    block_gym recoded

--- Wagon-2/08-About-blocks/block_gym.rb
+++ Wagon-2/08-About-blocks/block_gym.rb
@@ -1,7 +1,11 @@
 # Recoding inject iterator
 
+
 def inject(array, initial_value)
-  # your code goes here
+        sum = initial_value
+        a = 0
+        array.each { |x| sum += yield(a, x) }
+        sum
 end
 
 puts inject(1..100, 0) { |initial_value, element| element + initial_value } == 5050 # true
@@ -9,11 +13,11 @@ puts inject(1..100, 0) { |initial_value, element| element + initial_value } == 5
 # Block timer
 
 def timer_for
-  # your code goes here
+        start = Time.now
+        yield
+        puts Time.now - start
 end
 
 timer_for do 
   (1..100).each { |i| (1..100000).to_a.shuffle.sort }
 end
-
-# Should return around 2-3 seconds
