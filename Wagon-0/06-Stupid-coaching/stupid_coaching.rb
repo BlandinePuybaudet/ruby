c1a48543a32bbcad71e1185ae1ff4779d6594d7f
BlandinePuybaudet <blandinoscope@orange.fr>
2014-01-12 12:18:17 +0100

    trois version de stupid_coach

--- Wagon-0/06-Stupid-coaching/stupid_coaching.rb
+++ Wagon-0/06-Stupid-coaching/stupid_coaching.rb
@@ -1,10 +1,57 @@
-def stupid_coaching 
- puts "What's up kid"
- while true
- answer=gets.chomp 
- break if answer=="leave me alone"
- coach_respond(answer)
- end
+######  STUPID_COACH vrs UNLESS ################
+=begin
+def stupid_coach
+	puts "What are you doing son?"
+	answer = gets.chomp
+	unless answer == "I am going to work right now"
+		"I don't care son, get dressed and go to work!"
+	else
+		"You're a good boy!"
+	end
+end	
+puts stupid_coach
+=end
 
-# Calling the method
-stupid_coaching
+
+################ STUPID_COACH vrs IF #########################
+=begin
+
+def stupid_coaching 
+	puts "What are you doing son?"
+	answer = gets.chomp
+	if answer.include?("?") 
+	 	"Silly question, get dressed and go to work !!"
+	elsif answer == "I am going to work right now"
+	 	"you're good boy!"
+	elsif answer == "I am going to work right now".upcase
+		"I can fell your motivation son!"	
+ 	else
+ 		"I don't care son, get dressed and go to work!"
+	end
+end   	
+puts stupid_coaching
+=end
+
+
+
+############# STUPID_COACH version CASE ##############
+
+
+def stupid_coach
+	puts "What are you doing son?"
+	answer = gets.chomp
+	case answer 
+	when "I am going to work"
+		"You're a good boy! Take the subway and go to work"
+	when "I AM GOING TO WORK"
+	 "I can fell your motivation son!"
+	when /[?]/
+		"Silly question, take the subway and go to work!"
+	else
+		"I don't care son, get dressed and go to work!"
+	end
+end
+puts stupid_coach
+
+
+
