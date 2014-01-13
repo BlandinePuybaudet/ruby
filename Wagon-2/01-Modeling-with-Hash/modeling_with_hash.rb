abf6d5aa66cb70ebbec96d7d4cad9dcb9054bb43
BlandinePuybaudet <blandinoscope@orange.fr>
2014-01-13 15:50:50 +0100

    version finalisee de modeling_with_hash

--- Wagon-2/01-Modeling-with-Hash/modeling_with_hash.rb
+++ Wagon-2/01-Modeling-with-Hash/modeling_with_hash.rb
@@ -7,18 +7,20 @@ side = gets.chomp
 puts "quelle boisson?"
 beverage = gets.chomp
 
-def calories_in (a, b, c)
- menu = { "Cheese Burger" => 290, 
- 	"Big Mac" => 300,
- 	"Mc Bacon" => 400,
-  	"Royal Cheese" => 130,
-	"French fries" => 130,
- 	"Potatoes" => 130,
- 	"Coca" => 160,
- 	"Sprite" => 170 }
-menu[a] + menu[b] + menu[c]
+def calories_in(a, b, c)
+  menu = {"Cheese Burger" => 290, 
+			 	"Big Mac" => 300,
+			 	"Mc Bacon" => 400,
+			  "Royal Cheese" => 130,
+				"French fries" => 130,
+			 	"Potatoes" => 130,
+			 	"Coca" => 160,
+			 	"Sprite" => 170 
+				 }
+	menu[a] + menu[b] + menu[c]
 end
+
 nb_cal = calories_in(burger, side, beverage)
-puts " Votre menu vous apporte #{nb_cal} calories"
+puts " Il y a #{nb_cal} calories dans votre menu"
 
 
