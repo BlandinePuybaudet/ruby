27fe2bec8208d685d1482c2fa6f5a122d38bdee4
BlandinePuybaudet <blandinoscope@orange.fr>
2014-01-14 14:15:14 +0100

    horses-race new vrs

--- Wagon-2/Horses race/horses_race.rb
+++ Wagon-2/Horses race/horses_race.rb
@@ -5,7 +5,15 @@ h = {"Caramel" => 10,
 	"Belle pomme" => 9
 	}
 
+=begin
 v = h.sort_by {|k, v| v}
 #puts v[0..2]
 puts "the winners are :"
+puts v[0..2]
+=end
+
+######## une autre façon de faire ############
+ 
+v = h.sort {|a, b| a[1] <=> b[1]}
+puts "classement des trois premiers :"
 puts v[0..2]
